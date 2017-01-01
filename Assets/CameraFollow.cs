a1558913bc6757dde32402624966df3780d1f782
Jannik Michel <jannik.michel@mni.thm.de>
2017-01-01 22:34:49 +0100

    initialize

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;
+	public float cameraHeight = 20.0f;
+
+	void Update() {
+		Vector3 pos = player.transform.position;
+		pos.y += cameraHeight;
+		transform.position = pos;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float horSpeed;
+	public GameObject go;
+	private Rigidbody rb;
+
+	void Awake() {
+		Application.targetFrameRate = 60;
+	}
+	void Start ()
+	{
+		
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (-moveHorizontal * horSpeed, 0.0f,-speed);
+
+		rb.AddForce (movement*speed,ForceMode.Acceleration);
+	}
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Plane_001")
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+}
