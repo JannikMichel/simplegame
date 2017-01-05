a212fca6089dc20b5a8f3ea6a0eb6781302385d1
Jannik Michel <jannik.michel@mni.thm.de>
2017-01-05 18:12:27 +0100

    uigbij

---
+++ Assets/pause.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pause : MonoBehaviour {
+	bool pause_bool = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void pause_funk(){
+		if (pause_bool) {
+			Time.timeScale = 1;
+
+		} else {
+			Time.timeScale = 0;
+		
+		}
+		pause_bool = !pause_bool;
+	}
+}
